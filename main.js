6faa3380e9e6b6d3bb891c2762b48e8f83674e2a
greg <lorgnierg@yahoo.com>
2023-03-10 22:26:55 +0100

    adding add button js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("this is a test")
+
+const addButton = document.querySelector('.adding')
+const container = document.querySelector('#weather-container')
+
+addButton.addEventListener('click', event => {
+    const newWeatherCard = document.createElement("div");
+    newWeatherCard.classList.add('weather-card');
+    container.appendChild(newWeatherCard);
+});
