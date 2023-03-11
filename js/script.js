8fc5b8a64c3cea23a281a386ce2f826c58639ebe
Wuraim <lorgnierg@yahoo.com>
2023-03-11 16:15:08 +0100

    adding card creator and card remover

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-console.log("this is a test")
+const addButton = document.querySelector('.adding');
+const container = document.querySelector('#weather-container');
 
-const addButton = document.querySelector('.adding')
-const container = document.querySelector('#weather-container')
+const destroyCard = (event) => {
+    event.target.closest('div.weather-card').remove()
+}
 
 addButton.addEventListener('click', event => {
-    const newWeatherCard = document.createElement("div");
+    const newWeatherCard = document.createElement('div');
     newWeatherCard.classList.add('weather-card');
+
+    const newHeaderWeatherCard = document.createElement('div');
+    newHeaderWeatherCard.classList.add('headerWeatherCard');
+
+    const logoClose = document.createElement('span');
+    logoClose.classList.add('material-icons');
+    logoClose.classList.add('closeButton');
+    logoClose.addEventListener('click', destroyCard)
+    logoClose.innerText = 'close';
+
+    newHeaderWeatherCard.appendChild(logoClose);
+    newWeatherCard.appendChild(newHeaderWeatherCard);
+
+    const newBodyWeatherCard = document.createElement('div');
+    newBodyWeatherCard.classList.add('bodyWeatherCard');
+
+    newWeatherCard.appendChild(newBodyWeatherCard);
+
     container.insertBefore(newWeatherCard, addButton);
 });
